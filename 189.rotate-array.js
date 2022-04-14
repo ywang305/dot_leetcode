7992e9b905c3b02da287782caad0079d2665a0a9
Yao Wang <ydwang@icapitalnetwork.com>
2022-04-14 12:23:26 -0400

    array reverse trick

---
+++ 189.rotate-array.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=189 lang=javascript
+ *
+ * [189] Rotate Array
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function (nums, k) {
+  const reverse = (start, end) => {
+    while (start < end) {
+      [nums[start], nums[end]] = [nums[end], nums[start]];
+      start++;
+      end--;
+    }
+  };
+  k %= nums.length;
+  reverse(0, nums.length - 1);
+  reverse(0, k - 1);
+  reverse(k, nums.length - 1);
+};
+// @lc code=end
