3b8fd310856f0461c1401a78c4cc6379aba7b501
Yao <ydwang@icapitalnetwork.com>
2023-10-24 20:13:44 -0400

    sliding window hard on 76

---
+++ solutions/76.minimum-window-substring.rb
@@ -0,0 +1,31 @@
+# @param {String} s
+# @param {String} t
+# @return {String}
+def min_window(s, t)
+  need_tally = t.chars.tally
+  need_key_type = need_tally.keys.length
+
+  left = right = nil
+  ans = ''
+
+  s.each_char.with_index do |c, i|
+      if need_tally.key? c
+          left ||= i
+          right = i
+
+          need_tally[c] -= 1
+          need_key_type -= 1 if need_tally[c].zero?
+
+          while need_key_type.zero?
+              left_c = s[left]
+              if need_tally.key? left_c
+                  substr = s[left..right]
+                  ans = substr if (ans.empty? || substr.length < ans.length)
+                  need_key_type += 1 if (need_tally[left_c] += 1) == 1
+              end
+              left += 1
+          end
+      end
+  end
+  ans
+end
