cef7a64c0f3d5dee709f2a597e770ff2a060de72
Yao <ydwang@icapitalnetwork.com>
2024-07-08 15:37:53 -0400

    word distance

---
+++ solutions/244.shortest-word-distance-ii.rb
@@ -0,0 +1,26 @@
+class WordDistance
+  def initialize(words_dict)
+    @hash = words_dict.each.with_index.each_with_object(Hash.new { |h, k| h[k] = [] }) do |(c, i), a|
+      a[c] << i
+    end
+  end
+
+  def shortest(word1, word2)
+    pos1, pos2 = @hash.values_at(word1, word2)
+    i = j = 0
+    ans = Float::INFINITY
+    while i < pos1.size && j < pos2.size
+      ans = [ans, (pos1[i] - pos2[j]).abs].min
+      pos1[i] < pos2[j] ? (i += 1) : (j += 1)
+    end
+    while i < pos1.size
+      ans = [ans, (pos1[i] - pos2[j - 1]).abs].min
+      i += 1
+    end
+    while j < pos2.size
+      ans = [ans, (pos1[i - 1] - pos2[j]).abs].min
+      j += 1
+    end
+    ans
+  end
+end
